b2a2e210105e970b71c6807ef9715c6c77b97a31
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-10-22 21:06:46 -0400

    api: pull year from path when fetching season standings

--- internal/server/seasons.go
+++ internal/server/seasons.go
@@ -3,12 +3,18 @@ package server
 import (
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo"
 )
 
 func (s *Server) driverStandingsByYearHandler(c echo.Context) error {
-	standings, err := s.db.GetDriverStandingsByYear(c.Request().Context(), 2020)
+	year, err := strconv.Atoi(c.Param("year"))
+	if err != nil {
+		return c.String(http.StatusBadRequest, "Invalid year")
+	}
+
+	standings, err := s.db.GetDriverStandingsByYear(c.Request().Context(), int32(year))
 	if err != nil {
 		// TODO: build out error handling
 		log.Fatal("Things went bonk:", err)
